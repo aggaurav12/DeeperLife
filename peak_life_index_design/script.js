ed4966b7acc3d9d0984f2c80352e50f80b5aa8f9
aggaurav12 <gaurigaurav1221@gmail.com>
2025-02-16 23:26:34 +0530

    updating env file 10

--- peak_life_index_design/script.js
+++ peak_life_index_design/script.js
@@ -1,6 +1,7 @@
 // Import Firebase SDK 
 //import ENV from "./config.js"; 
-import ENV from "./firebase-config.js"; // Import secure Firebase config 
+//import ENV from "./firebase-config.js"; // Import secure Firebase config
+const ENV = window.ENV;  
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import {
