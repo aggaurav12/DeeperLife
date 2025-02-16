e5f33b8afdadb01d240622d589d40a367bcdeb17
aggaurav12 <gaurigaurav1221@gmail.com>
2025-02-16 21:40:25 +0530

    Updating based on Env Variable

--- peak_life_index_design/script.js
+++ peak_life_index_design/script.js
@@ -1,6 +1,15 @@
 // Import Firebase SDK 
-import ENV from "./config.js"; 
+//import ENV from "./config.js"; 
 //import { ENV } from "./public/config.js"; 
+const ENV = {
+  FIREBASE_API_KEY: import.meta.env.VITE_FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import {
   getFirestore,
