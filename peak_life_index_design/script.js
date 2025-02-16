00706a1793842c69d4e4ddc47e9f59b76e405ab8
aggaurav12 <gaurigaurav1221@gmail.com>
2025-02-16 22:57:35 +0530

    updating env file

--- peak_life_index_design/script.js
+++ peak_life_index_design/script.js
@@ -1,14 +1,7 @@
 // Import Firebase SDK 
 //import ENV from "./config.js"; 
 //import { ENV } from "./public/config.js"; 
-const ENV = {
-  FIREBASE_API_KEY: import.meta.env.VITE_FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  FIREBASE_PROJECT_ID: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID: import.meta.env.VITE_FIREBASE_APP_ID
-};
+const ENV = window.ENV;  
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import {
