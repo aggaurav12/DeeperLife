ebeeebac2c8f8a84a5b841f4166cfedb79c9ee3c
aggaurav12 <gaurigaurav1221@gmail.com>
2025-03-13 00:13:25 +0530

    Delete peak_life_index_design directory

--- peak_life_index_design/config.js
+++
@@ -1,8 +0,0 @@
-const ENV = {
-  FIREBASE_API_KEY: 'AIzaSyDW01wLzktmXaofcAcCGmg...',
-  FIREBASE_AUTH_DOMAIN: 'deeperlife-1221.firebaseapp.com',
-  FIREBASE_PROJECT_ID: 'deeperlife-1221',
-  FIREBASE_STORAGE_BUCKET: 'deeperlife-1221.firebasestorage.app',
-  FIREBASE_MESSAGING_SENDER_ID: '623918066735',
-  FIREBASE_APP_ID: '1:623918066735:web:fb35b1f0dc12c0a28caada'
-}; export default ENV;
